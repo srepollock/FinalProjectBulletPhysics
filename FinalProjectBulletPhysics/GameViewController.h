c6644f835c812dd1efb9a22dac9ce19d10966421
Spencer Pollock <srepollock@gmail.com>
2017-04-12 10:46:54 -0700

    Initial Commit

---
+++ FinalProjectBulletPhysics/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  FinalProjectBulletPhysics
+//
+//  Created by Spencer Pollock on 2017-04-12.
+//  Copyright © 2017 Spencer Pollock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
---
+++ FinalProjectBulletPhysics/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FinalProjectBulletPhysics
+//
+//  Created by Spencer Pollock on 2017-04-12.
+//  Copyright © 2017 Spencer Pollock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
